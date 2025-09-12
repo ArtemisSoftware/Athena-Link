1a5862b695e393e42ec82bd0ac59fcb15615c479
gustavo <gustavo.isaac@gmail.com>
2025-09-12 14:15:54 +0100

    DeepLinkUriPattern

---
+++ composeApp/src/androidMain/kotlin/org/artemissoftware/athenalink/navigation/DeepLinkUriPattern.kt
@@ -0,0 +1,7 @@
+package org.artemissoftware.athenalink.navigation
+
+object DeepLinkUriPattern {
+
+    const val DETAIL_PATTERN_1 = "https://athenalink.com/item/{id}"
+    const val DETAIL_PATTERN_2 = "olimpus://athenalink.com/item/{id}"
+}
